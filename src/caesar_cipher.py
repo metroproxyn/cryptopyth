db7dba482096ee4826f2dc74e00e3aa307e723ee
Alex Maszanski <39808040+Wreemde@users.noreply.github.com>
2023-03-28 14:05:03 +0400

    Added caesar_decrypt function for the Caesae cipher

--- src/caesar_cipher.py
+++ src/caesar_cipher.py
@@ -28,7 +28,26 @@ def caesar_encrypt(plaintext, shift):
 
 # ToDo
 # Write a decryptor, change caesar_cipher function name to the encryptor
-def caesar_decrypt(text, shift):
+def caesar_decrypt(ciphertext, shift):
     result = ""
     # iterate through each character in the input text
+    for char in ciphertext:
+        # check if character is an uppercase or lowercase letter
+        if char.isalpha():
+            # shift the character by the specified number of positions
+            char_code = ord(char) - shift
+            # handle wraparound for positions beyond 'Z' or 'z'
+            if char.isupper():
+                if char_code > ord('A'):
+                    char_code += 26
+            else:
+                if char_code < ord('a'):
+                    char_code += 26
+            
+            # append the shifted character to the result string
+            result += chr(char_code)       
+        else:
+            # append non-letter characters as-is
+            result += char
+            
     return result
